44ff88ae7a6faa725d91d097cbd53db3c76b5ba2
moudjane <mathis.oudjane@gmail.com>
2022-03-15 12:07:38 +0100

    upper-case

---
+++ upper-case.js
@@ -0,0 +1,24 @@
+'use strict';
+
+function upperCase(str) {
+    let arr = str.split('');
+    let output = [];
+    let temp;
+    for(let i = 0; i < arr.length; i++) {
+        if(arr[i].charCodeAt(0) >= 97 && arr[i].charCodeAt(0) <= 122) {
+            temp = String.fromCharCode((arr[i].charCodeAt(0) - 32))
+            output.push(temp)
+        } else {
+            output.push(arr[i])
+        }
+    }
+    return output.join('')
+}
+
+
+// It should log -> UHOO
+console.log(
+    upperCase('uhoo'),
+);
+
+// It should log ->
